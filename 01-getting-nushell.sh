0d54577912d87939c89de8ab4b9c77dff41e420c
Aleksandr Kovalev <aleksandr@kovalev.engineer>
2023-10-30 15:28:28 +0100

    skip interactive prompt in rustup installer

--- 01-getting-nushell.sh
+++ 01-getting-nushell.sh
@@ -3,9 +3,11 @@
 # the easiest way to get nushell is to use cargo
 # we will need rust toolchain anyway, so why not use it now
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rustup.sh
+chmod +x rustup.sh
+./rustup.sh -y
+rm rustup.sh
 
 cargo install nushell
 
 sudo chsh -s $(which nu) $USER
-
